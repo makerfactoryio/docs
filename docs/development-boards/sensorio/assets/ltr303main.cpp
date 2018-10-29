897facd835d067cc9e2f3f7b2a5ddc4e4f064d1d
Maciej Rogozinski <marogi@o2.pl>
2018-10-29 16:24:12 +0100

    First FW doc batch

---
+++ docs/development-boards/sensorio/assets/ltr303main.cpp
@@ -0,0 +1,37 @@
+#include "mbed_events.h"
+#include "Serial.h"
+#include "ltr303.h"
+
+mbed::Serial log(USBTX, USBRX, 115200);
+mbed::I2C iic(SENSORS_SDA, SENSORS_SCL);
+mbed::InterruptIn irq(LTR303_INT, PinMode::PullUp);
+events::EventQueue scheduler;
+
+Ltr303 sensor(iic, irq);
+
+void measDone(float);
+
+// callback from driver in separate execution context
+void dataCallback(float result)
+{
+    scheduler.call_in(500, measDone, result);
+}
+
+// executor in this context
+void measDone(float result)
+{
+    log.printf("Light sensor output: %5.3f lux.\n", result);
+    // request next measurement
+    sensor.get(dataCallback);
+}
+
+int main(int argc, char **argv)
+{
+    log.printf("Start\n");
+
+    // schedule first measurement, it will chain next in callback
+    sensor.get(dataCallback);
+
+    // running event loop
+    scheduler.dispatch_forever();
+}
--- docs/development-boards/sensorio/sensors.md
+++
@@ -1,9 +0,0 @@
-#Onboard Sensors 
-
-## BME680 - Environmental
-
-The Bosch Sensortec
-
-## BMA253 - Accelerometer
-
-## LTR303-ALS - Luminosity
