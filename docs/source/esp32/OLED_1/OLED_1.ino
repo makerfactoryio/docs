134bebab8292a0a33df54fd65410bb680b6e7cdd
Prof. Dr. Markus Stäuble <staeuble@hm.edu>
2018-11-13 09:31:49 +0100

    Updated OLED sketch

--- docs/source/esp32/OLED_1/OLED_1.ino
+++ docs/source/esp32/OLED_1/OLED_1.ino
@@ -22,8 +22,7 @@
   BSD license, check license.txt for more information
   All text above, and the splash screen must be included in any redistribution
 *********************************************************************/
-
-#include <Adafruit_SSD1306.h>
+#include "Adafruit_SSD1306.h"
 
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
