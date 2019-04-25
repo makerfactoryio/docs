c917bc2cac86da0d12468d9de19cc9e7057c9b49
GuentherBrenner <48586944+GuentherBrenner@users.noreply.github.com>
2019-04-25 10:24:43 +0200

    Add files via upload

--- docs/source/esp32/Buttons/Buttons.ino
+++ docs/source/esp32/Buttons/Buttons.ino
@@ -1,53 +1,46 @@
-/***************************************************************************************
+/******************************************************************
 Created with PROGRAMINO IDE for Arduino
-Project     : Maker-Factory ESP32 Board
-Libraries   :
-Author      : UlliS
-Description : Read the buttons
+Project     : Read menu buttons
+Board       : MAKERFACTORY EBB ESP32
+Version     : 1.0
+Date        : 28.12.2018
+Author      : MF
 
+Description : output the button status via the terminal
 
-Copyright (c) 2018 Conrad Electronic SE.
+Switches:
+MENU SW401 all ON
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-***************************************************************************************/
+License:
+This file may be redistributed under the terms of the MIT license.
+A copy of this license has been included with this distribution 
+in the file LICENSE.
+******************************************************************/
 
+// pin defines for the menu buttons
 #define BTN_UP      34
 #define BTN_DOWN    25
 #define BTN_LEFT    26
 #define BTN_RIGHT   32
 #define BTN_CENTER  27
 
-
 void setup()
 {
-    
+    // initialize the button pins as input without internal pullup resistor
     pinMode(BTN_UP,INPUT);
     pinMode(BTN_DOWN,INPUT);
     pinMode(BTN_LEFT,INPUT);
     pinMode(BTN_RIGHT,INPUT);
     pinMode(BTN_CENTER,INPUT);
     
+    // initialize the serial port
     Serial.begin(115200);
-    
 }
 
 void loop()
 {
-    
+    // endless loop
+    // output the button status via the terminal
     Serial.print("UP: ");
     Serial.print(digitalRead(BTN_UP));
     Serial.print(" | ");
@@ -67,6 +60,5 @@ void loop()
     Serial.print("CENTER: ");
     Serial.println(digitalRead(BTN_CENTER));
     
-    delay(250);
-    
+    delay(250);   
 }
